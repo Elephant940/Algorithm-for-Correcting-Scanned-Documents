c71b24c73042ad71a727928913df9dac23ff6c6b
Owen Schwartz <oschwartz10612@gmail.com>
2020-06-07 15:33:29 -0400

    Added if statement

--- main_and_ocr.py
+++ main_and_ocr.py
@@ -95,18 +95,19 @@ for entry in os.scandir(cwd):
                         #for individual files within the lists of files
                             for img_ext in img_exts:
                             #for each extension in the list of img_exts
+                                if filename.lower().endswith(img_ext):
                                 
-                                im = Image.open(subfolder + "/" + filename)
-                                #open the image file
-                            
-                                new_pdf_name = f"{filename}.pdf"
-                                #set the new_pdf_name equal to the pdf version of the image file
+                                    im = Image.open(subfolder + "/" + filename)
+                                    #open the image file
                                 
-                                pdf_ims.append(new_pdf_name)
-                                #add the pdf versions of the images to a list
-                                
-                                new_pdf = im.save(new_pdf_name)
-                                #convert the image file to a pdf
+                                    new_pdf_name = f"{filename}.pdf"
+                                    #set the new_pdf_name equal to the pdf version of the image file
+                                    
+                                    pdf_ims.append(new_pdf_name)
+                                    #add the pdf versions of the images to a list
+                                    
+                                    new_pdf = im.save(new_pdf_name)
+                                    #convert the image file to a pdf
 
                     elif filename.lower().endswith(img_ext):
                     #if the file has a listed image extension
